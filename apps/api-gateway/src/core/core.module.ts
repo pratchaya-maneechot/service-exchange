f5a7e44241ef248d00bc8ceab3183eed1f398d68
pratchaya maneechot <pratya240960@gmail.com>
2025-07-24 17:03:59 +0700

    feat(api-gateway): apply LineModule

--- apps/api-gateway/src/core/core.module.ts
+++ apps/api-gateway/src/core/core.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
+import { LineModule } from './line/line.module';
 @Module({
-  imports: [UserModule, HealthModule, AuthModule],
+  imports: [UserModule, HealthModule, AuthModule, LineModule],
 })
 export class CoreModule {}
